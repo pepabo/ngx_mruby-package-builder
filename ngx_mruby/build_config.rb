d0a2fe47a40e2eb946603c2c076ac74168e6a217
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-08-14 22:20:29 +0900

    initialized ngx_mruby rpm builder for centos7

---
+++ ngx_mruby/build_config.rb
@@ -0,0 +1,25 @@
+MRuby::Build.new do |conf|
+
+  toolchain :gcc
+
+  conf.gembox 'full-core'
+
+  conf.gem :github => 'iij/mruby-io'
+  conf.gem :github => 'iij/mruby-env'
+  conf.gem :github => 'iij/mruby-dir'
+  conf.gem :github => 'iij/mruby-digest'
+  conf.gem :github => 'iij/mruby-process'
+  conf.gem :github => 'iij/mruby-pack'
+  conf.gem :github => 'iij/mruby-socket'
+  conf.gem :github => 'mattn/mruby-json'
+  conf.gem :github => 'mattn/mruby-onig-regexp'
+  conf.gem :github => 'matsumoto-r/mruby-memcached'
+  conf.gem :github => 'matsumoto-r/mruby-sleep'
+  conf.gem :github => 'matsumoto-r/mruby-userdata'
+  conf.gem :github => 'matsumoto-r/mruby-uname'
+  conf.gem :github => 'matsumoto-r/mruby-mutex'
+  conf.gem :github => 'matsumoto-r/mruby-cache'
+
+  # ngx_mruby extended class
+  conf.gem '../mrbgems/ngx_mruby_mrblib'
+end
