d76159fa778b61d9345997b967306b9c5d0fa065
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-11-18 21:33:56 +0900

    added commented mrbgem

--- ngx_mruby/build_config.rb
+++ ngx_mruby/build_config.rb
@@ -14,6 +14,7 @@ MRuby::Build.new do |conf|
   conf.gem :github => 'mattn/mruby-json'
   conf.gem :github => 'mattn/mruby-onig-regexp'
   conf.gem :github => 'matsumoto-r/mruby-redis'
+  # conf.gem :github => 'matsumoto-r/mruby-memcached'
   conf.gem :github => 'matsumoto-r/mruby-sleep'
   conf.gem :github => 'matsumoto-r/mruby-userdata'
   conf.gem :github => 'matsumoto-r/mruby-uname'
