d20dff0eef00b761a95c8b527c98e97e228ddd5e
Teppei Yano <buckelwal@gmail.com>
2020-02-16 16:56:36 +0900

    remove mrbgems bundled as core library
    - They are bundled with mruby 1.4.1 or later

--- ngx_mruby/build_config.rb
+++ ngx_mruby/build_config.rb
@@ -8,13 +8,10 @@ MRuby::Build.new do |conf|
     cc.flags << '-fPIE'
   end
 
-  conf.gem :github => 'iij/mruby-io'
   conf.gem :github => 'iij/mruby-env'
   conf.gem :github => 'iij/mruby-dir'
   conf.gem :github => 'iij/mruby-digest'
   conf.gem :github => 'iij/mruby-process'
-  conf.gem :github => 'iij/mruby-pack'
-  conf.gem :github => 'iij/mruby-socket'
   conf.gem :github => 'mattn/mruby-json'
   conf.gem :github => 'mattn/mruby-onig-regexp'
   conf.gem :github => 'matsumoto-r/mruby-redis'
